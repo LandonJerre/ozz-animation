584d1c5fe87456a95149e294d2fb488685387708
LandonJerre <david.bachrati@gmail.com>
2018-07-28 18:12:56 +0200

    Readded RH to LH conversion into fbx converter

--- src/animation/offline/fbx/fbx.cc
+++ src/animation/offline/fbx/fbx.cc
@@ -250,13 +250,14 @@ FbxSystemConverter::FbxSystemConverter(const FbxAxisSystem& _from_axis,
                                        const FbxSystemUnit& _from_unit) {
   // Build axis system conversion matrix.
   const math::Float4x4 from_matrix = BuildAxisSystemMatrix(_from_axis);
-
+  const math::Float4x4 to_matrix = {
+      ozz::math::simd_float4::x_axis(), ozz::math::simd_float4::y_axis(),
+      -ozz::math::simd_float4::z_axis(), ozz::math::simd_float4::w_axis()};
   // Finds unit conversion ratio to meters, where GetScaleFactor() is in cm.
   const float to_meters =
       static_cast<float>(_from_unit.GetScaleFactor()) * .01f;
-
   // Builds conversion matrices.
-  convert_ = Invert(from_matrix) *
+  convert_ = to_matrix * Invert(from_matrix) *
              math::Float4x4::Scaling(math::simd_float4::Load1(to_meters));
   inverse_convert_ = Invert(convert_);
   inverse_transpose_convert_ = Transpose(inverse_convert_);
